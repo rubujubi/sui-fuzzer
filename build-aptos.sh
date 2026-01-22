8c29ebcd6eb1575f9adc1e136012ae3d8bae3b9e
Ru Ji <ru.ji@movementlabs.xyz>
2026-01-22 18:15:05 -0500

    fix version compatibility

--- build-aptos.sh
+++ build-aptos.sh
@@ -4,8 +4,9 @@ set -e
 # Copy Aptos-specific Cargo.toml
 cp Cargo-aptos.toml Cargo.toml
 
-# Remove Cargo.lock to avoid conflicts
-rm -f Cargo.lock
+# Copy over Cargo.lock from aptos-core to ensure dependency consistency
+cp aptos-core/Cargo.lock Cargo.lock
+
 
 # Build with Aptos features
 cargo build --release --features aptos
