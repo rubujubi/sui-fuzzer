f901db7d02f7e0528a9fd2a5a4324da922a4f231
Ru Ji <ru.ji@movementlabs.xyz>
2026-01-22 21:53:39 -0500

    fix script

--- build-aptos.sh
+++ build-aptos.sh
@@ -7,6 +7,9 @@ cp Cargo-aptos.toml Cargo.toml
 # Copy over Cargo.lock from aptos-core to ensure dependency consistency
 cp aptos-core/Cargo.lock Cargo.lock
 
+# Copy cargo config to enable tokio_unstable flag (required for disable_lifo_slot)
+mkdir -p .cargo
+cp aptos-core/.cargo/config.toml .cargo/config.toml
 
 # Build with Aptos features
 cargo build --release --features aptos
